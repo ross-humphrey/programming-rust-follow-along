12cc92912dbefbe39a7dcf4aa8e274b8153632b0
candyflossuk <ross.a.humphrey@googlemail.com>
2022-01-07 13:48:23 +0000

    Added some examples of arrays and vectors - removed targets

--- scratch/target/debug/scratch.d
+++
@@ -1,1 +0,0 @@
-/Users/rossa/Documents/Engineering/Rust/programming-rust-follow-along/scratch/target/debug/scratch: /Users/rossa/Documents/Engineering/Rust/programming-rust-follow-along/scratch/src/main.rs
